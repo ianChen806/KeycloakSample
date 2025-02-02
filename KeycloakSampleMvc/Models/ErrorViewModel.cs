5961ef47d4eceba3da6d7ad8bd20e1500222f1aa
IanChen <ian806.chen@gmail.com>
2025-02-02 15:49:49 +0800

    add mvc project

---
+++ KeycloakSampleMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KeycloakSampleMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
